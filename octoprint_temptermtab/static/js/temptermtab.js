e2786e4d8bceb957034f1d98f3d78885cd367465
jneilliii <jneilliii+github@gmail.com>
2018-04-06 18:35:39 -0400

    test

--- octoprint_temptermtab/static/js/temptermtab.js
+++ octoprint_temptermtab/static/js/temptermtab.js
@@ -8,11 +8,11 @@ $(function() {
 			//Check for TouchUIPlugin, if not loaded apply special class.
 			var htmlId = $("html").attr("id");
 			if (htmlId != "touch") {
-				$('div#temp').append($('div#term').removeClass('tab-pane').css({'min-height':'125px','max-height':'125px','height':'125px'}));
+				$('div#temp').append($('div#term').removeClass('tab-pane'));
 				$('li#term_link').hide();
 				if(!$('#terminal-filterpanel input[type=checkbox]:eq(0)').prop('checked')){$('#terminal-filterpanel input[type=checkbox]:eq(0)').trigger('click');}
 				if(!$('#terminal-filterpanel input[type=checkbox]:eq(2)').prop('checked')){$('#terminal-filterpanel input[type=checkbox]:eq(2)').trigger('click');}
-				$('pre#terminal-output').on('DOMNodeInserted',function(){if($('button').filter(function(index){return $(this).text() === "Autoscroll";}).hasClass('active')){$(this).scrollTop($(this)[0].scrollHeight);}});
+				$('pre#terminal-output').css({'min-height':'125px','max-height':'125px','height':'125px'}).on('DOMNodeInserted',function(){if($('button').filter(function(index){return $(this).text() === "Autoscroll";}).hasClass('active')){$(this).scrollTop($(this)[0].scrollHeight);}});
 			}
 		}
 	}
